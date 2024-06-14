4dea69f41ce31a11a64c70e8caeeae9411ce04b4
Kadee Nicole Lawrence <108096710+KNLawrence01@users.noreply.github.com>
2024-06-14 14:48:44 -0500

    Update multiQC.sh
    edited tar commands and fixed mistype

--- multiQC.sh
+++ multiQC.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 echo "tar multiQC ..."
-tar -xzf MultiQC.tar.gz
-cd MultiQC
+tar -xf MultiQC.tar.gz
 
 echo "tar qualimap ..."
-for f in *qualimap.tar.gz; do tar -xcf $f; done
+for f in *qualimap.tar.gz; do tar -xf $f; done
 
 echo "running multiqc..."
 multiqc -ds .
 
 mv multiqc_report.html multiqc_data/
 mv multiqc_data/ multiqc-report
-tar -czvf multiqc-report.tar.gz umltiqc-report/
+tar -czvf multiqc-report.tar.gz multiqc-report/
 mv multiqc-report.tar.gz ../
