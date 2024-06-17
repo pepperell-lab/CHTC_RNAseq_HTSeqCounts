a665f0761193ba7715a2412a0069255e89d73f92
Kadee Nicole Lawrence <108096710+KNLawrence01@users.noreply.github.com>
2024-06-17 10:20:02 -0500

    Update bwaMEM.sh
    edited to run 1 paired read sample at a time

--- bwaMEM.sh
+++ bwaMEM.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-bwa index $1
-bwa mem -M -t 8 $1 $2_1P.fq.gz $2_2P.fq.gz > $2.sam
+bwa index MtbNCBIH37Rv.fa
+bwa mem -M -t 8 MtbNCBIH37Rv.fa 5075_10_S1_1P.fq.gz 5075_10_S1_2P.fq.gz > 5075_10_S1.sam
