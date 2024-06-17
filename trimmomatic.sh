77a69a91e77acc85cf43d1fd6afed9f5c2392485
Kadee Nicole Lawrence <108096710+KNLawrence01@users.noreply.github.com>
2024-06-17 10:17:05 -0500

    Update trimmomatic.sh
    edited to run 1 paired end sample

--- trimmomatic.sh
+++ trimmomatic.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-trimmomatic PE $1_R1_001.fastq.gz $1_R2_001.fastq.gz -baseout $1.fq.gz -threads 4 ILLUMINACLIP:adapters.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:50
+trimmomatic PE 5075_10_S1_R1_001.fastq.gz 5075_10_S1_R2_001.fastq.gz -baseout 5075_10_S1.fq.gz -threads 4 ILLUMINACLIP:adapters.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:50
