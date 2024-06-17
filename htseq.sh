b8b47c403e311e0950626b15d30eb3a730ff6615
Kadee Nicole Lawrence <108096710+KNLawrence01@users.noreply.github.com>
2024-06-17 10:28:20 -0500

    Update htseq.sh
    edited to run 1 paired end sample at a time

--- htseq.sh
+++ htseq.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 -m HTSeq.scripts.count -f bam -r pos -t gene --idattr=gene_name --nonunique none -s reverse $1.sort.bam $2 > $1_HTSeqCounts.txt
+python3 -m HTSeq.scripts.count -f bam -r pos -t gene --idattr=gene_name --nonunique none -s reverse 5075_10_S1.sort.bam MtbNCBIH37Rv.gtf > 5075_10_S1_HTSeqCounts.txt
