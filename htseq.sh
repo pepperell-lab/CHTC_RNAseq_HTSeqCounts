97d4851202074152558ebf920aba5b5815dad4d1
Kadee Nicole Lawrence <108096710+KNLawrence01@users.noreply.github.com>
2024-07-01 13:31:22 -0500

    Update htseq.sh
    Changed to match the og

--- htseq.sh
+++ htseq.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 -m HTSeq.scripts.count -f bam -r pos -t gene --idattr=gene_name --nonunique none -s reverse $1.sort.bam $2 > $1_HTSeqCounts.txt
+python3 -m HTSeq.scripts.count -f bam -r pos -t gene -i gene_name --nonunique none -s reverse $1.sort.bam $2 > $1_HTSeqCounts.txt
