889113ec229ae258ee36e1fde087dccd86ddc341
Marissa Zhang <yzhang2587@wisc.edu>
2024-08-02 10:17:23 -0500

    add format HTseqCounts

--- htseq.sh
+++ htseq.sh
@@ -2,3 +2,4 @@
 
 gunzip -c $1.sort.bam.gz > $1.sort.bam
 python3 -m HTSeq.scripts.count -f bam -r pos -t gene -i gene_name --nonunique none -s reverse $1.sort.bam $2 > $1_HTSeqCounts.txt
+python3 format_HTSeqCounts.py $1_HTSeqCounts.txt $1_HTSeqCounts.txt
