737d87268b7c8f2f179b4b5b08001d2dcf3e0eb2
Marissa Zhang <113387963+Marissa-working@users.noreply.github.com>
2025-02-03 16:24:21 -0600

    rm space when assigning variable value in bwaMEM.sh

--- bwaMEM.sh
+++ bwaMEM.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-staging_prefix = /staging/groups/pepperell_group/Mtb_RNAseq/HTSeqCounts
-results_staging_prefix = /staging/groups/pepperell_group/Mtb_RNAseq/HTSeqCounts/Results
+staging_prefix=/staging/groups/pepperell_group/Mtb_RNAseq/HTSeqCounts
+results_staging_prefix=/staging/groups/pepperell_group/Mtb_RNAseq/HTSeqCounts/Results
 cp $results_staging_prefix/$2_1P.fq.gz $results_staging_prefix/$2_2P.fq.gz $staging_prefix/$1 ./
 
 bwa index $1
