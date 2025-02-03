b182205e9b5204bd8c44b8b3d147325be1ed8f7a
Sophie Liu <sophieliu@Sophies-MacBook-Air-8.local>
2025-02-03 15:06:38 -0600

    update staging usage of sam_view

--- samtools_view.sh
+++ samtools_view.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+staging_prefix = /staging/groups/pepperell_group/Mtb_RNAseq/HTSeqCounts
+results_staging_prefix = /staging/groups/pepperell_group/Mtb_RNAseq/HTSeqCounts/Results
+multiqc_staging_prefix = /staging/groups/pepperell_group/Mtb_RNAseq/HTSeqCounts/Results/MultiQC
+
+cp $results_staging_prefix/$1.sam.gz ./
 
 #samtools index $1.sam
 gunzip -c $1.sam.gz > $1.sam
 samtools view -bhSu $1.sam | gzip > $1.bam.gz
+
+mv $1.bam.gz $results_staging_prefix/
+rm $results_staging_prefix/$1.sam.gz
