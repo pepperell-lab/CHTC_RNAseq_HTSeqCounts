c7f829b18aa37b9804059425bc6635442a9df4f1
Sophie Liu <sophieliu@Sophies-MacBook-Air-8.local>
2025-02-03 14:47:46 -0600

    Update /staging usage for fastqc2

---
+++ fastqc2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+results_staging_prefix = /staging/groups/pepperell_group/Mtb_RNAseq/HTSeqCounts/Results
+multiqc_staging_prefix = /staging/groups/pepperell_group/Mtb_RNAseq/HTSeqCounts/Results/MultiQC
+
+cp $results_staging_prefix/$1_1P.fq.gz $results_staging_prefix/$1_2P.fq.gz ./
+fastqc $1_1P.fq.gz $1_2P.fq.gz -t 4
+
+mv $1_1P_fastqc.zip $multiqc_staging_prefix/
+mv $1_2P_fastqc.zip $multiqc_staging_prefix/
