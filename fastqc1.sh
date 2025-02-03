490031aa641fc8e215056fea7c29de2b8b5e7031
Marissa Zhang <113387963+Marissa-working@users.noreply.github.com>
2025-02-03 16:27:54 -0600

    Fix typo in fastqc1.sh

--- fastqc1.sh
+++ fastqc1.sh
@@ -6,4 +6,4 @@ cp $staging_prefix/$1_R1_001.fastq.gz $staging_prefix/$1_R2_001.fastq.gz ./
 fastqc $1_R1_001.fastq.gz $1_R2_001.fastq.gz -t 4
 
 mv $1_R1_001_fastqc.zip $multiqc_staging_prefix/
-mv $2_R2_001_fastqc.zip $multiqc_staging_prefix/
+mv $1_R2_001_fastqc.zip $multiqc_staging_prefix/
