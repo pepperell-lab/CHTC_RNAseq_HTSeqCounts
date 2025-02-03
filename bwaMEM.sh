3ea08821e1e054bff0f74d80e25f9f6e3efc29e0
Sophie Liu <sophieliu@Sophies-MacBook-Air-8.local>
2025-02-03 15:00:59 -0600

    Update /staging usage for BWA

--- bwaMEM.sh
+++ bwaMEM.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+staging_prefix = /staging/groups/pepperell_group/Mtb_RNAseq/HTSeqCounts
+results_staging_prefix = /staging/groups/pepperell_group/Mtb_RNAseq/HTSeqCounts/Results
+cp $results_staging_prefix/$2_1P.fq.gz $results_staging_prefix/$2_2P.fq.gz $staging_prefix/$1 ./
 
 bwa index $1
 bwa mem -M -t 8 $1 $2_1P.fq.gz $2_2P.fq.gz | gzip > $2.sam.gz
+
+mv $2.sam.gz $results_staging_prefix/
+rm $results_staging_prefix/$2_1P.fq.gz $results_staging_prefix/$2_2P.fq.gz
