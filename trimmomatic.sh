ed211fed0c0a3075fc4d5691f195580c1022f1e2
Marissa Zhang <113387963+Marissa-working@users.noreply.github.com>
2025-02-03 16:40:07 -0600

    rm space when assigning value in trimmomatic.sh

--- trimmomatic.sh
+++ trimmomatic.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-staging_prefix = /staging/groups/pepperell_group/Mtb_RNAseq/HTSeqCounts
-results_staging_prefix = /staging/groups/pepperell_group/Mtb_RNAseq/HTSeqCounts/Results
+staging_prefix=/staging/groups/pepperell_group/Mtb_RNAseq/HTSeqCounts
+results_staging_prefix=/staging/groups/pepperell_group/Mtb_RNAseq/HTSeqCounts/Results
 cp $staging_prefix/$1_R1_001.fastq.gz $staging_prefix/$1_R2_001.fastq.gz $staging_prefix/adapters.fa ./
 
 trimmomatic PE $1_R1_001.fastq.gz $1_R2_001.fastq.gz -baseout $1.fq.gz -threads 4 ILLUMINACLIP:adapters.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:50
