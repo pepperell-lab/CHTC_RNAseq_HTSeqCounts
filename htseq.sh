be129c9ef5252db0100493e923671c500845f594
Marissa Zhang <113387963+Marissa-working@users.noreply.github.com>
2025-02-03 16:35:36 -0600

    format_HTSeqCounts.py come from transfer instead of /staging in htseq.sh

--- htseq.sh
+++ htseq.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 staging_prefix=/staging/groups/pepperell_group/Mtb_RNAseq/HTSeqCounts
 results_staging_prefix=/staging/groups/pepperell_group/Mtb_RNAseq/HTSeqCounts/Results
-cp format_HTSeqCounts.py $results_staging_prefix/$1.sort.bam.gz $staging_prefix/$2 ./
+cp $results_staging_prefix/$1.sort.bam.gz $staging_prefix/$2 ./
 
 gunzip -c $1.sort.bam.gz > $1.sort.bam
 python3 -m HTSeq.scripts.count -f bam -r pos -t gene -i gene_name --nonunique none -s reverse $1.sort.bam $2 > $1_HTSeqCounts.txt
