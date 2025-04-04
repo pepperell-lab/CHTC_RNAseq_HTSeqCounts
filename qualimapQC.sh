1efc33c0b3b1bb32f45cc0bef34d4c83e6c1145a
Marissa Zhang <yzhang2587@wisc.edu>
2025-04-04 11:00:04 -0500

    fix staging transfer in quallimap

--- qualimapQC.sh
+++ qualimapQC.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+staging_prefix=file:///staging/groups/pepperell_group/Mtb_RNAseq/HTSeqCounts
+results_staging_prefix=file:///staging/groups/pepperell_group/Mtb_RNAseq/HTSeqCounts/Results
+multiqc_staging_prefix=file:///staging/groups/pepperell_group/Mtb_RNAseq/HTSeqCounts/Results/MultiQC
+cp $results_staging_prefix/$1.sort.bam.gz $staging_prefix/$2 ./
+
 gunzip -c $1.sort.bam.gz > $1.sort.bam
 # run qualimap-bamqc
 qualimap bamqc -bam $1.sort.bam -outdir $1_bamqc --java-mem-size=2G
@@ -7,3 +12,5 @@ qualimap bamqc -bam $1.sort.bam -outdir $1_bamqc --java-mem-size=2G
 qualimap rnaseq -bam $1.sort.bam -outdir $1_rnaseq -pe -gtf $2 --java-mem-size=2G
 
 tar -zcvf $1.qualimap.tar.gz $1_bamqc/ $1_rnaseq/
+
+mv $1.qualimap.tar.gz $multiqc_staging_prefix/
