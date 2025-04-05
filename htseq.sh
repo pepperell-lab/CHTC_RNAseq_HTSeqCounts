be5b03136ce70f13e540988a904aa635254128aa
Marissa Zhang <yzhang2587@wisc.edu>
2025-04-05 12:46:05 -0500

    fix: rm files after both qualimap and htseq done

--- htseq.sh
+++ htseq.sh
@@ -8,4 +8,3 @@ python3 -m HTSeq.scripts.count -f bam -r pos -t gene -i gene_name --nonunique no
 python3 format_HTSeqCounts.py $1_HTSeqCounts.txt $1_HTSeqCounts.txt
 
 mv $1_HTSeqCounts.txt $results_staging_prefix/
-rm format_HTSeqCounts.py $results_staging_prefix/$1.sort.bam.gz 
